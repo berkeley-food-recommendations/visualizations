d984de775d994fe7067541e31d5c505c60a4e047
Seshadri Mahalingam <seshadri@berkeley.edu>
2012-12-06 09:59:30 -0800

    seed 200-m matching file

--- restaurants/app/models/restaurant_tweet.rb
+++ restaurants/app/models/restaurant_tweet.rb
@@ -1,20 +1,19 @@
 class RestaurantTweet < ActiveRecord::Base
   attr_accessible :restaurant, :tweet, :username
 
- def self.rests_in_common(rest)
+  def self.rests_in_common(rest)
     tweets = self.find_all_by_restaurant(rest)
     rests = {}
     tweets.each do |tweeter|
-        user_tweets = self.find_all_by_username(tweeter.username)
-        user_tweets.each do |user_tweet|
-            if rests.has_value?(user_tweet.restaurant)
-                rests[user_tweet.restaurant] += 1
-            else
-                rests[user_tweet.restaurant] = 1
-            end
+      user_tweets = self.find_all_by_username(tweeter.username)
+      user_tweets.each do |user_tweet|
+        if rests.has_value?(user_tweet.restaurant)
+          rests[user_tweet.restaurant] += 1
+        else
+          rests[user_tweet.restaurant] = 1
         end
+      end
     end
     return rests
   end
 end
-
